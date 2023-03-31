5446b34a51bb923f3f58426d90da3c9510e843b0
Tomáš Bouška <tomas@buvis.net>
2023-03-31 15:47:54 +0200

    Prepare for authentik automation

---
+++ cluster-home/operations/security/authentik/configuration/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    authentik = {
+      source  = "goauthentik/authentik"
+      version = "2023.3.0"
+    }
+    sops = {
+      source = "carlpett/sops"
+      version = "0.7.2"
+    }
+  }
+}
